36f127270963ed645fea0fb96991ab92ab4b68bb
Luka Borak <44951292+BorakL@users.noreply.github.com>
2020-07-24 12:43:26 +0200

    Update zadatak2.js

--- Nedelja2/Nedelja/zadatak2.js
+++ Nedelja2/Nedelja/zadatak2.js
@@ -7,9 +7,13 @@
 
 
 for(let i=1; i<=100; i++){
-    if(i%3==0 && i%5==0) console.log("FizzBuzz");
-    else if(i%3==0 && i%5!=0) console.log("Fizz");
-    else if(i%3!=0 && i%5==0) console.log("Buzz");
-    else if(i%3!=0 && i%5!=0) console.log(i);
+    let result="";
+    if(i%3==0) result += "Fizz";
+    if(i%5==0) result += "Buzz";
+    if(result === ""){
+        console.log(i);
+    }
+    else {
+        console.log(result);
+    }
 }
-
